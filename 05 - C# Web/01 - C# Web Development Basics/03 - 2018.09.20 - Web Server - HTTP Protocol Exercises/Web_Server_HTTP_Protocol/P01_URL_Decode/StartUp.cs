16e47bc3d7d0401ecc66aa8e79dea5bcff652163
Joro <iliev_g@outlook.com>
2018-09-25 22:53:12 +0300

    Added part of the HTTP Protocol exercise

---
+++ 05 - C# Web/01 - C# Web Development Basics/03 - 2018.09.20 - Web Server - HTTP Protocol Exercises/Web_Server_HTTP_Protocol/P01_URL_Decode/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace P01_URL_Decode
+{
+    using System;
+    using System.Net;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string encodedURL = Console.ReadLine();
+            string decodedURL = WebUtility.UrlDecode(encodedURL);
+            Console.WriteLine(decodedURL);
+        }
+    }
+}
---
+++ 05 - C# Web/01 - C# Web Development Basics/03 - 2018.09.20 - Web Server - HTTP Protocol Exercises/Web_Server_HTTP_Protocol/P02_Validate_URL/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace P02_Validate_URL
+{
+    using System;
+    using System.Linq;
+    using System.Net;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                string encodedURL = Console.ReadLine();
+                string decodedURL = WebUtility.UrlDecode(encodedURL);
+
+                Uri uri = new Uri(decodedURL);
+                string uriScheme = uri.Scheme;
+                string uriHost = uri.Host;
+                int uriPort = uri.Port;
+                string uriPath = uri.AbsolutePath;
+                string uriQuery = uri.Query;
+                string uriFragment = uri.Fragment;
+
+                Console.WriteLine(uriScheme);
+                Console.WriteLine(uriHost);
+                Console.WriteLine(uriPort);
+                Console.WriteLine(uriPath);
+                Console.WriteLine(uriQuery);
+                Console.WriteLine(uriFragment);
+            }
+        }
+    }
+}
