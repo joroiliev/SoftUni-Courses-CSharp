94bacd5bc658ba932d10baee1e529b90cb243cc4
Joro <iliev_g@outlook.com>
2018-09-26 23:52:20 +0300

    Finished solution to second problem from HTTP Protocol exercise

--- 05 - C# Web/01 - C# Web Development Basics/03 - 2018.09.20 - Web Server - HTTP Protocol Exercises/Web_Server_HTTP_Protocol/P02_Validate_URL/StartUp.cs
+++ 05 - C# Web/01 - C# Web Development Basics/03 - 2018.09.20 - Web Server - HTTP Protocol Exercises/Web_Server_HTTP_Protocol/P02_Validate_URL/StartUp.cs
@@ -7,25 +7,57 @@
     {
         public static void Main()
         {
-            while (true)
+            try
             {
-                string encodedURL = Console.ReadLine();
-                string decodedURL = WebUtility.UrlDecode(encodedURL);
+                Uri uri = GetURI();
 
-                Uri uri = new Uri(decodedURL);
                 string uriScheme = uri.Scheme;
                 string uriHost = uri.Host;
-                int uriPort = uri.Port;
+                int uriPort = CheckIfPortValid(uri);
                 string uriPath = uri.AbsolutePath;
                 string uriQuery = uri.Query.TrimStart('?');
                 string uriFragment = uri.Fragment.TrimStart('#');
 
-                Console.WriteLine(uriScheme);
-                Console.WriteLine(uriHost);
-                Console.WriteLine(uriPort);
-                Console.WriteLine(uriPath);
-                Console.WriteLine(uriQuery);
-                Console.WriteLine(uriFragment);
+                PrintOutput(uriScheme, uriHost, uriPort, uriPath, uriQuery, uriFragment);
+            }
+            catch (UriFormatException)
+            {
+                Console.WriteLine("Invalid URL");
+            }
+        }
+
+        private static Uri GetURI()
+        {
+            string encodedURL = Console.ReadLine();
+            string decodedURL = WebUtility.UrlDecode(encodedURL);
+            Uri uri = new Uri(decodedURL);
+            return uri;
+        }
+
+        private static int CheckIfPortValid(Uri uri)
+        {
+            if ((uri.Scheme == "http" && uri.Port != 80) ||
+                (uri.Scheme == "https" && uri.Port != 443))
+            {
+                throw new UriFormatException();
+            }
+
+            return uri.Port;
+        }
+
+        private static void PrintOutput(string uriScheme, string uriHost, int uriPort, string uriPath, string uriQuery, string uriFragment)
+        {
+            Console.WriteLine($"Protocol: {uriScheme}");
+            Console.WriteLine($"Host: {uriHost}");
+            Console.WriteLine($"Port: {uriPort}");
+            Console.WriteLine($"Path: {uriPath}");
+            if (uriQuery != string.Empty)
+            {
+                Console.WriteLine($"Query: {uriQuery}");
+            }
+            if (uriFragment != string.Empty)
+            {
+                Console.WriteLine($"Fragment: {uriFragment}");
             }
         }
     }
