b2924b24251b810a06e71ed442e7203b006a0edf
Joro <iliev_g@outlook.com>
2018-10-02 09:45:46 +0300

    Added some problems from Async lab

---
+++ 05 - C# Web/01 - C# Web Development Basics/04 - Web Server - Asynchronous Processing Lab/Solution/P01_Even_Numbers_Thread/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace P01_Even_Numbers_Thread
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] range = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Thread evens = new Thread(() => PrintEvenNumbers(range[0], range[1]));
+            evens.Start();
+            evens.Join();
+            Console.WriteLine("Thread finished working");
+        }
+
+        private static void PrintEvenNumbers(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
